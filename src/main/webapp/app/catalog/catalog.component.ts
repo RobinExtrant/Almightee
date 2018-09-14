c2022b38212dcdac812ee5b74892196c052b0649
RobinExtrant <robin.extrant@gmail.com>
2018-09-14 15:44:50 +0200

    Pagination dans le catalogue et préparation du bouton pour l'ajout du motif au panier

--- src/main/webapp/app/catalog/catalog.component.ts
+++ src/main/webapp/app/catalog/catalog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { PatternService } from './../entities/pattern/pattern.service';
-import { Pattern } from './../shared/model/pattern.model';
+import { Pattern, IPattern } from './../shared/model/pattern.model';
 
 @Component({
     selector: 'jhi-catalog',
@@ -8,7 +9,11 @@ import { Pattern } from './../shared/model/pattern.model';
     styles: []
 })
 export class CatalogComponent implements OnInit {
+    currentPage: number;
+    totalPages: number;
+    totalItems: number;
     patterns: Pattern[] = [];
+    itemsPerPage: number;
     err: any;
 
     constructor(private patternService: PatternService) {}
@@ -19,10 +24,48 @@ export class CatalogComponent implements OnInit {
       {name: 'Vie', author: 'Vincent', imageURL: 'badUrl1', price: 100000},
       {name: 'Ordi', author: 'Alfred', imageURL: 'fakeUrl2', price: 500},
       {name: 'Prise', author: 'Mathieu', imageURL: 'badUrl1', price: 0.23}
-      ];*/
+      ];
 
         this.patternService.query().subscribe(patternsRes => {
             this.patterns = patternsRes.body;
-        });
+        });*/
+        this.currentPage = 1;
+        this.itemsPerPage = 6;
+        this.load(this.currentPage - 1, this.itemsPerPage);
+    }
+
+    loadAll() {
+        this.patternService.query().subscribe((res: HttpResponse<IPattern[]>) => this.paginatePatterns(res.body, res.headers));
+        return;
+    }
+
+    next() {
+        this.currentPage = this.currentPage + 1;
+        this.load(this.currentPage - 1, this.itemsPerPage);
+    }
+
+    previous() {
+        this.currentPage = this.currentPage - 1;
+        this.load(this.currentPage - 1, this.itemsPerPage);
+    }
+
+    load(pageToSearch: number, numberItems: number) {
+        this.patternService
+            .query({
+                page: pageToSearch,
+                size: numberItems
+            })
+            .subscribe((res: HttpResponse<IPattern[]>) => this.paginatePatterns(res.body, res.headers));
+        return;
+    }
+
+    private paginatePatterns(data: IPattern[], headers: HttpHeaders) {
+        this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+        this.totalPages = this.totalItems / this.itemsPerPage;
+        this.patterns = data;
+    }
+
+    addToCart(pattern: Pattern) {
+        console.log('On ajoute ici le motif ' + pattern.name + ' au panier');
     }
 }
